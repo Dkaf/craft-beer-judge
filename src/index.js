ca316556f088e302c175ae567a57eecad5f5bc05
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-05 23:51:52 -0500

    app now renders with state component parent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import AppState from './app-state';
+import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+import './index.css';
+
+
+const target = document.getElementById('root');
+
+render(
+  <AppState>
+    <App />
+  </AppState>,
+  target,
+  registerServiceWorker()
+)
