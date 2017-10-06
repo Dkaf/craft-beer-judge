38dd418a5d64d3e59edd97dd0f951a3cb303cb2b
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-06 11:04:25 -0500

    header text added in state

--- src/app-state.js
+++ src/app-state.js
@@ -4,6 +4,9 @@ class AppState extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      header: {
+        text: 'Beer Fridge'
+      },
       username: '',
       email: '',
       password: '',
