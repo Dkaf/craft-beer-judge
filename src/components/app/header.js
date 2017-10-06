439f410b72a0a9b99f6c2f5887ca6f0261461e99
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-06 11:06:27 -0500

    added route and redirect to dashboard

--- src/components/app/header.js
+++ src/components/app/header.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Redirect } from 'react-router-dom';
 import Home from './../home';
-import About from './../about';
+import Dashboard from './../dashboard';
 // import SideNav from './../side-nav';
-import SearchBar from './../search-bar';
+import SearchBar from './search-bar';
 
 import './../../styles/header.css';
 
@@ -19,20 +19,23 @@ class Header extends Component {
         <div className="container main-container">
           <header id="header">
             <SearchBar state={state} setAppState={this.props.setAppState} />
-            <h1 id="title">Beer Fridge</h1>
+            <h1 id="title">{state.header.text}</h1>
           </header>
             {/* <SideNav appState={state} setAppState={this.props.setAppState} naviconClass={state.sideNav.naviconClass} menuClass={state.sideNav.menuClass} /> */}
-            <Route path='/' render={ props =>(
+            <Route path='/' render={ props => (
+              state.loggedin ? (
+                <Redirect to='/dashboard' />
+              ) :
               <Home appState={state} setAppState={this.props.setAppState} />
             )} exact/>
-            <Route path='/about' render={ props => (
-              <About appState={state} />
+            <Route path='/dashboard' render={ props => (
+              <Dashboard appState={state} setAppState={this.props.setAppState} />
             )} />
           <footer className="footer">
             <Link className="router-link" to="/">
               <i className="fa fa-home fa-2x" aria-hidden="true"></i>
             </Link>
-            <Link className="router-link" to="/about">
+            <Link className="router-link" to="/dashboard">
               <i className="fa fa-user-o fa-2x" aria-hidden="true"></i>
             </Link>
           </footer>
