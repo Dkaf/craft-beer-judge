1885f90fe53564b5ecb5be59937cce0ccef874e0
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-06 11:05:57 -0500

    moved login and signup to home folder

---
+++ src/components/home/login.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './../../styles/login.css';
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.formSubmit = this.formSubmit.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+
+  formSubmit(e) {
+    e.preventDefault();
+    e.target.reset();
+    let options = {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+      username: this.props.appState.email,
+      password: this.props.appState.password})
+    };
+    let request = new Request('https://shielded-brook-50392.herokuapp.com/api/login', options);
+    fetch(request)
+      .then( res => res.json())
+      .then(json => {
+        if(json.success === true ) {
+          this.props.setAppState(() => {
+            let newState = Object.assign({}, this.props.state);
+            newState.jwt = json.token;
+            newState.loggedIn = true;
+            return newState;
+          })
+          console.log(this.props.appState);
+        }
+      })
+      .catch( err => {
+        console.log(err);
+      });
+  }
+
+  changeHandler(e) {
+    const target = e.target;
+    if(target.name === 'email-address') {
+      this.props.setAppState({email: target.value})
+    }
+    else if(target.name === 'password') {
+      this.props.setAppState({password: target.value})
+    }
+  }
+
+  render() {
+
+    return (
+      <main className="container login-container pa4">
+      <form onSubmit={this.formSubmit} className="measure center">
+        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+          <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+          <div className="mt3">
+            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+            <input className="pa2 b--teal input-reset ba bg-transparent w-100 grey" onChange={this.changeHandler} type="email" name="email-address"  id="email-address" />
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+            <input className="b pa2 b--teal input-reset ba bg-transparent w-100 grey" onChange={this.changeHandler} type="password" name="password"  id="password" />
+          </div>
+        </fieldset>
+        <div className="">
+          <input className="b ph3 pv2 input-reset ba b--teal bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+        </div>
+        <div className="lh-copy mt3">
+          {/* <a href="#0" className="f6 link dim db">Forgot your password?</a> */}
+        </div>
+      </form>
+    </main>
+    
+    )
+  }
+}
+
+export default Login
---
+++ src/components/home/signup.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './../../styles/login.css';
+
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.formSubmit = this.formSubmit.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+
+  formSubmit(e) {
+    e.preventDefault();
+    let options = {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+      username: this.props.appState.email.trim().toLowerCase(),
+      password: this.props.appState.password})
+    };
+    let request = new Request('https://shielded-brook-50392.herokuapp.com/api/signup', options);
+    if(this.props.appState.password !== this.props.appState.verifyPassword) {
+      return this.props.setAppState({matchError: true});
+    } else {
+    e.target.reset();      
+    fetch(request)
+      .then( res => res.json())
+      .then(json => {
+        console.log(json);
+        this.props.setAppState(() => {
+          let newState = Object.assign({}, this.props.appState);
+          newState.jwt = json.token;
+          newState.loggedIn = true;
+          return newState;
+        })
+      })
+      .catch( err => {
+        console.log(err);
+      });
+    }
+  }
+
+  changeHandler(e) {
+    const target = e.target;
+    if(target.name === 'email-address') {
+      this.props.setAppState({email: target.value})
+    }
+    else if(target.name === 'password') {
+      this.props.setAppState({password: target.value})
+    } else if(target.name === 'verify-password') {
+      this.props.setAppState({verifyPassword: target.value})
+    }
+  }
+
+  render() {
+    const state = this.props.appState;
+    let errDisplay = state.matchError ? <span className="match-error">Passwords do not match</span> : null;
+
+    return (
+      <main className="container login-container pa4">
+      <form onSubmit={this.formSubmit} className="measure center">
+        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+          <legend className="f4 fw6 ph0 mh0">Sign Up</legend>
+          <div className="mt3">
+            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+            <input className="pa2 b--teal input-reset ba bg-transparent w-100" onChange={this.changeHandler} type="email" name="email-address"  id="email-address" />
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+            <input className="b pa2 b--teal input-reset ba bg-transparent w-100" onChange={this.changeHandler} type="password" name="password"  id="password" />
+            <label className="db fw6 lh-copy f6" htmlFor="verify-password">Verify Password</label>
+            {errDisplay}
+            <input className="b pa2 b--teal input-reset ba bg-transparent w-100" onChange={this.changeHandler} type="password" name="verify-password"  id="verify-password" />
+          </div>
+        </fieldset>
+        <div className="">
+          <input className="b ph3 pv2 input-reset ba b--teal bg-transparent grow pointer f6 dib" type="submit" value="Sign up" />
+        </div>
+        <div className="lh-copy mt3">
+        </div>
+      </form>
+    </main>
+    )
+  }
+}
+
+export default SignUp;
