78b0d7fa2afc059a62d220819fb598e342bf6948
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-10 14:43:07 -0500

    will find beer in search results on component mount

--- src/components/beer-card/index.js
+++ src/components/beer-card/index.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class BeerCard extends Component {
+
+  componentWillMount() {
+    let newState = Object.assign({}, this.props.appState);
+    
+    this.props.appState.search.searchResults.find((beer) => {
+      if (beer.id === this.props.match.params.id) {
+        newState.selectedBeer = beer
+      };
+      return this.props.setAppState(() => {
+        return newState;
+      });
+    })
+  }
+
   render() {
-    const state = this.props.appState
+    const selectedBeer = this.props.appState.selectedBeer
+    const 
     return (
       <div>
-        <h2 className="beer-name">{state.beerName}</h2>
-        <img className="beer-label" src={state.beerLabel} alt="beer label" />
-        <span className="beer-style">{state.beerStyle}</span>
-        <p className="beer-description">{state.beerDescription}</p>
+        <h2 className="beer-name">{selectedBeer.name}</h2>
+        <img className="beer-label" src={selectedBeer.labels.medium ? selectedBeer.labels.medium : ''} alt="beer label" />
+        <span className="beer-style">{selectedBeer.style.shortName}</span>
+        <p className="beer-description">{selectedBeer.description ? selectedBeer.description : 'no description available'}</p>
       </div>
     );
   }
