2f703cdb5d7b462d85ed4ac02039d4c0d9981675
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-10 14:40:11 -0500

    added link to beer around arrow container

--- src/components/search-item.js
+++ src/components/search-item.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './../styles/search-item.css'
 
 class SearchItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    let path=`/beer/${this.props.id}`;
     return (
-      <div>
-        <div className="arrow-container">
-          <i className="fa fa-angle-right" aria-hidden="true"></i>
-        </div>
-        <div className="search-item-container">
+      <div className="container search-item-container">
+        <div className="info-container">
           <span className="item-name">{this.props.name}</span>
-          <span className="item-type">{this.props.type}</span>   
-        </div>     
+          {/* <span className="item-type">{this.props.type}</span>    */}
+        </div>
+          <Link to={path}>
+            <div className="arrow-container">
+              <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
+            </div>
+          </Link>
       </div>
     );
   }
