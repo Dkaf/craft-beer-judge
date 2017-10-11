3195208434f4855b6dfc5340da0a7e798c1a7a31
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 18:08:40 -0500

    will now fetch search data from the server from url params

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -2,18 +2,55 @@ import React, { Component } from 'react';
 import SearchItem from './search-item';
 
 class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    this.mapArray = this.mapArray.bind(this);
+    this.fetchSearchResults = this.fetchSearchResults.bind(this);
+  }
+
+  mapArray(array) {
+      return array.map( (beer, i) => {
+        return <SearchItem key={i} labels={beer.labels} id={beer.id} name={beer.name} type={beer.style.name} path={this.props.path}/>
+    });
+  };
+
+  fetchSearchResults() {
+    if (this.props.path === '/beer') {
+      let options = {
+        method: 'GET'
+      };
+      let url = 'https://shielded-brook-50392.herokuapp.com/api/beers/' + this.props.match.params.query;
+      let request = new Request(url, options);
+      fetch(request)
+      .then( data => data.json())
+      .then( results => {
+        this.props.setAppState(() => {
+          let newState = Object.assign({}, this.props.appState);
+          newState.search.searchResults = results.data.data;
+          newState.loading = false;
+          return newState;
+        });
+      }).catch( err => {console.log(err)});
+    }
+  }
+
+  componentDidMount() {
+    this.fetchSearchResults()
+  }
+
 
   render() {
-
-    let results = this.props.array.map( (beer, i) => {
-      return <SearchItem key={i} labels={beer.labels} id={beer.id} name={beer.name} type={beer.style.name} path={this.props.path}/>
-    })
-
-    return (
-      <div className="container results-container">
-        {results} 
-      </div>
-    );
+    if (this.props.array === []) {
+      return (
+        <div></div>
+      )
+    } else {
+      return (
+        <div className="container results-container">
+          {this.mapArray(this.props.array)} 
+        </div>
+      );
+    }
   }
 }
 
