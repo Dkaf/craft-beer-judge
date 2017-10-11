60ff33a9a5af306ace9b2779ec10280138b32f83
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 18:09:11 -0500

    removed unused import

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import AppState from './../../app-state';
 import Header from './header';
-import Dashboard from './../dashboard'
 import './../../styles/App.css';
 
 class App extends Component {
