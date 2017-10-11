ed5e65df574da61a9982b8625c57d04ede4de37d
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 11:52:38 -0500

    results now maps prop array

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -4,8 +4,8 @@ import SearchItem from './search-item';
 class SearchResults extends Component {
   render() {
 
-    let results = this.props.appState.search.searchResults.map( (beer, i) => {
-      return <SearchItem key={i} beer={beer} id={beer.id} name={beer.name} type={beer.style.name}/>
+    let results = this.props.array.map( (beer, i) => {
+      return <SearchItem key={i} beer={beer} id={beer.id} name={beer.name} type={beer.style.name} path={this.props.path}/>
     })
 
     return (
