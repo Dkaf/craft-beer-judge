472a7c4d438550cbbd23f1b0c922de6525222eae
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 11:53:48 -0500

    edited to search prop array

--- src/components/beer-card/index.js
+++ src/components/beer-card/index.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
+import AddBeer from './add-beer';
+import './../../styles/beer-card.css'
 
 class BeerCard extends Component {
 
   componentWillMount() {
     let newState = Object.assign({}, this.props.appState);
-    
-    this.props.appState.search.searchResults.find((beer) => {
-      if (beer.id === this.props.match.params.id) {
-        newState.selectedBeer = beer
-      };
-      return this.props.setAppState(() => {
-        return newState;
-      });
-    })
+      this.props.array.find((beer) => {
+        if (beer.name === this.props.match.params.name) {
+          newState.selectedBeer = beer;
+          }
+          return this.props.setAppState(() => {
+            return newState
+          });
+        })
   }
 
   render() {
-    const selectedBeer = this.props.appState.selectedBeer
-    const 
+    const state = this.props.appState
+    const label = state.selectedBeer.labels ? state.selectedBeer.labels.medium : '#'
+    let showButton = state.selectedBeer.rating ? <span>Your rating: {state.selectedBeer.rating}/10</span> : <AddBeer appState={state} setAppState={this.props.setAppState} />;
     return (
-      <div>
-        <h2 className="beer-name">{selectedBeer.name}</h2>
-        <img className="beer-label" src={selectedBeer.labels.medium ? selectedBeer.labels.medium : ''} alt="beer label" />
-        <span className="beer-style">{selectedBeer.style.shortName}</span>
-        <p className="beer-description">{selectedBeer.description ? selectedBeer.description : 'no description available'}</p>
+      <div className="beer-info-container">
+        <h2 className="beer-name">{state.selectedBeer.name}</h2>
+        <img className="beer-label" src={label} alt="beer label" />
+        {/* <span className="beer-style">{state.selectedBeer.style.shortName}</span> */}
+        <p className="beer-description">{state.selectedBeer.description ? state.selectedBeer.description : 'no description available'}</p>
+        <AddBeer appState={state} setAppState={this.props.setAppState} />
       </div>
     );
   }
