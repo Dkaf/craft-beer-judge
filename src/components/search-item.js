b11fe1ca0d0bb1783de44344c3e367d1d9ba78e2
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 18:09:00 -0500

    removed unused constructor

--- src/components/search-item.js
+++ src/components/search-item.js
@@ -3,16 +3,13 @@ import { Link } from 'react-router-dom';
 import './../styles/search-item.css'
 
 class SearchItem extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
       <div className="container search-item-container">
         <Link to={this.props.path + `/${this.props.name}`} className="container search-item-link">
         <div className="info-container">
-          {this.props.labels ? <img src={this.props.labels.icon} /> : null }
+          {this.props.labels ? <img src={this.props.labels.icon} alt="beer label" /> : null }
           <span className="item-name">{this.props.name}</span>
           {/* <span className="item-type">{this.props.type}</span>    */}
         </div>
