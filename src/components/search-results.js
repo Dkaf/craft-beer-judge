bc953a68c3aebbad62d902a825d0b7f0d02c7e45
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 14:37:04 -0500

    added beer labels to props

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import SearchItem from './search-item';
 
 class SearchResults extends Component {
+
   render() {
 
     let results = this.props.array.map( (beer, i) => {
-      return <SearchItem key={i} beer={beer} id={beer.id} name={beer.name} type={beer.style.name} path={this.props.path}/>
+      return <SearchItem key={i} labels={beer.labels} id={beer.id} name={beer.name} type={beer.style.name} path={this.props.path}/>
     })
 
     return (
