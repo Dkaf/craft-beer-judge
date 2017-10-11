a15071cffc67760b8d2411358753953d8e6c8d41
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 14:36:37 -0500

    link now wraps info container

--- src/components/search-item.js
+++ src/components/search-item.js
@@ -10,13 +10,16 @@ class SearchItem extends Component {
   render() {
     return (
       <div className="container search-item-container">
+        <Link to={this.props.path + `/${this.props.name}`} className="container search-item-link">
         <div className="info-container">
+          {this.props.labels ? <img src={this.props.labels.icon} /> : null }
           <span className="item-name">{this.props.name}</span>
           {/* <span className="item-type">{this.props.type}</span>    */}
         </div>
-          <Link to={this.props.path + `/${this.props.name}`} className="arrow-container">
+          <div className="arrow-container">
             <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
-          </Link>
+          </div>
+        </Link>
       </div>
     );
   }
