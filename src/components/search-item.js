5d2474cd22a3910abaa6bedb1480eb397a12bce4
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 11:51:48 -0500

    changed link path to a prop

--- src/components/search-item.js
+++ src/components/search-item.js
@@ -8,17 +8,14 @@ class SearchItem extends Component {
   }
 
   render() {
-    let path=`/beer/${this.props.id}`;
     return (
       <div className="container search-item-container">
         <div className="info-container">
           <span className="item-name">{this.props.name}</span>
           {/* <span className="item-type">{this.props.type}</span>    */}
         </div>
-          <Link to={path}>
-            <div className="arrow-container">
-              <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
-            </div>
+          <Link to={this.props.path + `/${this.props.name}`} className="arrow-container">
+            <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
           </Link>
       </div>
     );
