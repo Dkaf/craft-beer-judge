e77e94225263ae655b4e22c168dcf348c37ff549
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-11 18:10:50 -0500

    assigned vars to check selectedBeer for data

--- src/components/beer-card/index.js
+++ src/components/beer-card/index.js
@@ -4,33 +4,41 @@ import './../../styles/beer-card.css'
 
 class BeerCard extends Component {
 
-  componentWillMount() {
+  componentDidMount() {
     let newState = Object.assign({}, this.props.appState);
-      this.props.array.find((beer) => {
-        if (beer.name === this.props.match.params.name) {
-          newState.selectedBeer = beer;
-          }
-          return this.props.setAppState(() => {
-            return newState
-          });
-        })
+    this.props.array.find((beer) => {
+      if (beer.name === this.props.match.params.name) {
+        newState.selectedBeer = beer;
+      }
+      return this.props.setAppState(() => {
+        return newState
+      });
+    });
   }
 
   render() {
     const state = this.props.appState
-    const label = state.selectedBeer.labels ? state.selectedBeer.labels.medium : '#'
-    let showButton = state.selectedBeer.rating ? <span>Your rating: {state.selectedBeer.rating}/10</span> : <AddBeer appState={state} setAppState={this.props.setAppState} />;
-    return (
-      <div className="beer-info-container">
-        <h2 className="beer-name">{state.selectedBeer.name}</h2>
-        <img className="beer-label" src={label} alt="beer label" />
-        <span className="beer-style">{state.selectedBeer.style.name}</span>
-        <p className="beer-description">{state.selectedBeer.description ? state.selectedBeer.description : 'no description available'}</p>
-        <span className="beer-abv">abv:{state.selectedBeer.abv}</span>
-        <span className="beer-glass">glassware:{state.selectedBeer.glass.name}</span>
-        {showButton}
-      </div>
-    );
+    const label = state.selectedBeer.labels ? state.selectedBeer.labels.medium : '#';
+    const style = state.selectedBeer.style ? state.selectedBeer.style.name : '';
+    const glass = state.selectedBeer.glass ? state.selectedBeer.glass.name : '';
+    let showButton = state.selectedBeer.rating ? <span className="your-rating">Your rating: {state.selectedBeer.rating}/10</span> : <AddBeer appState={state} setAppState={this.props.setAppState} />;
+    if(this.props.appState.selectedBeer === {}) {
+      return <div></div>
+    } else {
+      return (
+        <div className="container beer-info-container">
+          <div className="container beer-info-inside-wrapper">
+            <h2 className="beer-name">{state.selectedBeer.name}</h2>
+            <img className="beer-label" src={label} alt="beer label" />
+            <h3 className="beer-style">{style}</h3>
+            <p className="beer-description">{state.selectedBeer.description ? state.selectedBeer.description : 'no description available'}</p>
+            <span className="beer-abv">abv: {state.selectedBeer.abv}</span>
+            {<span className="beer-glass">glassware: {glass}</span>}
+            {showButton}
+          </div>
+        </div>
+      );
+    }
   }
 }
 
