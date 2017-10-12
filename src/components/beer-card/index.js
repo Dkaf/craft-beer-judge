8847bce737fa083da82ac43294cd6257cdb03b4b
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-12 13:21:27 -0500

    now searches fridge first

--- src/components/beer-card/index.js
+++ src/components/beer-card/index.js
@@ -4,22 +4,53 @@ import BackArrow from './../back-arrow';
 import './../../styles/beer-card.css'
 
 class BeerCard extends Component {
+  constructor(props) {
+    super(props);
+    this.searchArray = this.searchArray.bind(this);
+    this.update = this.update.bind(this);
+  }
 
-  componentDidMount() {
-    let newState = Object.assign({}, this.props.appState);
-    this.props.array.find((beer) => {
+  update(newState) {
+    this.props.setAppState(() => {
+      return Object.assign({}, this.props.appState, newState);
+    })
+  }
+
+  searchArray(firstArr, secondArr) {
+    let found = true;
+    let val;
+    firstArr.find((beer) => {
       if (beer.name === this.props.match.params.name) {
-        newState.selectedBeer = beer;
+        return (
+          val = {selectedBeer: beer},
+          found = true
+        );
+      } else {
+        return found = false;
       }
-      return this.props.setAppState(() => {
-        return newState
-      });
     });
+    if (secondArr && !found) {
+      secondArr.find( (beer) => {
+        if (beer.name === this.props.match.params.name) {
+          return val = {selectedBeer: beer};
+        } else {
+          return null;
+        }
+      });
+    }
+    return this.update(val);
+  }
+  componentDidMount() {
+    if(this.props.array === this.props.appState.search.searchResults && this.props.appState.loggedIn === true) {
+      this.searchArray(this.props.appState.fridge, this.props.array);
+    } else {
+      this.searchArray(this.props.array);
+    }
   }
 
   render() {
     const state = this.props.appState
-    const label = state.selectedBeer.labels ? state.selectedBeer.labels.medium : '#';
+    const label = state.selectedBeer.labels ? state.selectedBeer.labels : '#';
     const style = state.selectedBeer.style ? state.selectedBeer.style.name : '';
     const glass = state.selectedBeer.glass ? state.selectedBeer.glass.name : '';
     let showButton = state.selectedBeer.rating ? <span className="your-rating">Your rating: {state.selectedBeer.rating}/10</span> : <AddBeer appState={state} setAppState={this.props.setAppState} />;
@@ -31,7 +62,11 @@ class BeerCard extends Component {
           <BackArrow history={this.props.history} />
           <div className="container beer-info-inside-wrapper">
             <h2 className="beer-name">{state.selectedBeer.name}</h2>
-            <img className="beer-label" src={label} alt="beer label" />
+            <picture>
+              <source srcSet={label.large} media="(min-width:768px)" />
+              <source srcSet={label.medium} media="(max-width: 600px)" />
+              <img className="beer-label" srcSet={label.medium} alt="beer label" />
+            </picture>
             <h3 className="beer-style">{style}</h3>
             <p className="beer-description">{state.selectedBeer.description ? state.selectedBeer.description : 'no description available'}</p>
             <span className="beer-abv">abv: {state.selectedBeer.abv}</span>
