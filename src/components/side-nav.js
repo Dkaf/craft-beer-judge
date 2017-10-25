d40a03361cea8b7297a376e40d606c9992a5b66a
Dkaf <Daniel.j.kafer@gmail.com>
2017-10-25 16:53:49 -0500

    changed class changes to boolean

--- src/components/side-nav.js
+++ src/components/side-nav.js
@@ -11,25 +11,23 @@ class SideNav extends Component {
 
   onClick(e) {
     (e.target.id === 'navicon') && e.preventDefault();
-    let clicked = (this.props.naviconClass === 'navicon') ? 'navicon clicked' : 'navicon';
-    let hidden = (this.props.menuClass === 'hidden') ? '' : 'hidden';
-    console.log(this.props.appState);
+    let clicked = (this.props.naviconClicked === false) ? true : false;
+    let hidden = (this.props.menuHidden === true) ? false : true;
     this.props.setAppState(() => {
       return (
-        this.props.appState.sideNav.naviconClass = clicked,
-        this.props.appState.sideNav.menuClass = hidden
+        this.props.appState.sideNav.naviconClicked = clicked,
+        this.props.appState.sideNav.menuHidden = hidden
       )
     });
   }
   render() {
     return (
-      <div>
-        <button id="navicon" className={this.props.naviconClass} onClick={this.onClick}></button>
-        <div id="menu" className={this.props.menuClass}>
+      <div className="side-nav">
+        <button id="navicon" className={this.props.naviconClicked ? 'navicon clicked' : 'navicon'} onClick={this.onClick}></button>
+        <div id="menu" className={this.props.menuHidden ? 'hidden' : ''}>
           <ul id="menu-list">
             <li className="menu-item"><Link to="/" onClick={this.onClick}>Home</Link></li>
             <li className="menu-item">Beer</li>
-            <li className="menu-item">About</li>
             <li className="menu-item"><Link to="/about" onClick={this.onClick}>About</Link></li>
           </ul>
         </div>
